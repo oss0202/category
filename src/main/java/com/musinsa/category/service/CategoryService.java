86871647eac1d357455aa51b62b557e5fdd3a159
Oh <oss0202@naver.com>
2021-11-13 16:09:05 +0900

    정렬 테스트 추가

--- src/main/java/com/musinsa/category/service/CategoryService.java
+++ src/main/java/com/musinsa/category/service/CategoryService.java
@@ -29,6 +29,7 @@ public class CategoryService {
             return categoryRepository.findAll()
                     .stream()
                     .map(CategoryResponseDto::new)
+                    .sorted()
                     .collect(Collectors.toList());
     }
 }
